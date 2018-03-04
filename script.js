46dc796fa78ea4e51009f1cf163953ce3398c12b
Lauren D <lefdoughty@gmail.com>
2018-03-03 21:14:02 -0500

    scrolling
    so I forgot I changed branches. This was all changes to the scrolling.

--- script.js
+++ script.js
@@ -1,42 +1,24 @@
-var section2Scroll = document.getElementById("section2");
-var topPos = section2Scroll.offsetTop
-var elem3 = document.getElementById('section3');
-var elem3Pos = elem3.offsetTop
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-document.getElementById('scrollButtonTo3').onclick = function () {
-   console.log('click')
-  scrollTo(document.getElementById('container'), topPos-30, 600);
-}
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-document.getElementById('scrollButtonTo3B').onclick = function () {
-   console.log('click')
-  scrollTo(document.getElementById('container'), elem3Pos-30, 600);
-}
+      // Store hash
+      var hash = this.hash;
 
-function scrollTo(element, to, duration) {
-    var start = element.scrollTop,
-        change = to - start,
-        currentTime = 0,
-        increment = 20;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
 
-    var animateScroll = function(){
-        currentTime += increment;
-        var val = Math.easeInOutQuad(currentTime, start, change, duration);
-        element.scrollTop = val;
-        if(currentTime < duration) {
-            setTimeout(animateScroll, increment);
-        }
-    };
-    animateScroll();
-}
-
-//t = current time
-//b = start value
-//c = change in value
-//d = duration
-Math.easeInOutQuad = function (t, b, c, d) {
-    t /= d/2;
-    if (t < 1) return c/2*t*t + b;
-    t--;
-    return -c/2 * (t*(t-2) - 1) + b;
-};
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
