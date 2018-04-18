9a015492724e555cfb2d115d193c9c5b2a208224
lgainsbrook <lauren.gainsbrook@gmail.com>
2018-04-18 18:39:10 -0400

    Organize JavaScript code

--- script.js
+++ script.js
@@ -1,149 +1,6 @@
 /*
 ==================================================
 
-Section 1 rotating text
-
-==================================================
-*/
-
-// TRYING TO FIGURE OUT THE setTimeout FUNCTION (Why doesn't the for/while loop wait until timeout is done before continuing???)
-
-rotatingDisplay();
-
-function rotatingDisplay() {
-    var i = 1;
-    while (i <= 1) {
-        /* display: block - current icon/text pair */
-        setDisplay(i);
-        /* wait 3 seconds */
-        /* display: none - current icon/text pair */
-        // setTimeout(function() {
-        //     document.getElementById("section1").style.display="none";
-        //     }, 3000);
-        i++;
-    }
-}
-
-function setDisplay(i) {
-        var icon = document.getElementById("icon" + i);
-        icon.style.display = "block";
-        var text = document.getElementById("text" + i);
-        text.style.display = "block";
-}
-
-function removeDisplay(i) {
-    document.getElementById("section1").style.display="none";
-
-    // var icon = document.getElementById("icon" + i);
-    // icon.style.display = "none";
-    // var text = document.getElementById("text" + i);
-    // text.style.display = "none";
-}
-
-
-
-/*
-==================================================
-
-Section 6 Navbar
-
-==================================================
-*/
-const section6Nav = document.getElementById('section6-nav');
-
-function changeBackground(event) {
-    let lis = section6Nav.getElementsByTagName('li');
-    for (let i = 0; i < lis.length; i++) {
-        lis[i].style.backgroundColor = 'white';
-    }
-    let parentTarget = event.target.parentElement;
-    if (parentTarget.tagName != 'LI') {
-        parentTarget = parentTarget.parentElement;
-    }
-    parentTarget.style.backgroundColor = 'gold';
-    displayContent(parentTarget);
-}
-
-function displayContent(parentTarget) {
-    var section6 = document.getElementById('section6');
-    var subSections = section6.getElementsByTagName('section');
-    for (let i = 0; i < subSections.length; i++) {
-        let sectionId = subSections[i].id;
-        if (parentTarget.id == sectionId + '-nav') {
-            document.getElementById(sectionId).style.display = 'flex';
-        }
-        else {
-            document.getElementById(sectionId).style.display = 'none';
-        }
-    }
-}
-
-/*
-==================================================
-
-Section 5 Navbar
-
-==================================================
-*/
-
-var sec5Nav = document.getElementById('section5-nav');        // reference to navbar element node
-
-function changeSec5Background(e) {
-  var sec5 = document.getElementById('current-projects');                 // reference to section 5 element node
-  var sec5NavTabs = sec5Nav.getElementsByTagName('li');           // NodeList for section 5 navbar tabs
-  var sec5SubSections = sec5.getElementsByTagName('section');     // NodeList for section 5 subsections
-  var target = e.target;
-  var elParent = target.parentNode;
-  for (let i = 0; i < sec5NavTabs.length; i++) {
-    if ((target.id == sec5NavTabs[i].id) || (elParent.id == sec5NavTabs[i].id)) {     // If ID of targer OR target's parent element (if user clicks on image)
-      sec5NavTabs[i].className = 'nav5-itemsClear';                                   // change background color of subsection's tab
-      sec5SubSections[i].className = 'section5ContentContainer'                       // render active subsection's html content visible
-      sec5.className = sec5SubSections[i].id + 'Bckgd';                               // modify's class to change to active subsection's background image
-    } else {                                                                          // Otherwise:
-      sec5NavTabs[i].className = 'nav5-itemsBlue';                                    // apply default color to inactive navbar tabs
-      sec5SubSections[i].className = 'sec5Hidden';                                    // render inactive subsections' HTML hidden
-    }
-  }
-}
-
-/*
-==================================================
-
-Scrolling
-
-==================================================
-*/
-
-
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
-
-
-/*
-==================================================
-
 Carousel Styling
 
 ==================================================
@@ -188,10 +45,97 @@ function changeCarouselColors(e) {
 
             // SF Removed default case because it leads to <li> elements triggering carousel color change
         }
-  }
+    }
 }
 
-/* --- Listen for hash change ---  */
+
+/*
+==================================================
+
+Scrolling
+
+==================================================
+*/
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
+/*
+==================================================
+
+Section 1 rotating text
+
+==================================================
+*/
+
+// TRYING TO FIGURE OUT THE setTimeout FUNCTION (Why doesn't the for/while loop wait until timeout is done before continuing???)
+
+rotatingDisplay();
+
+function rotatingDisplay() {
+    var i = 1;
+    while (i <= 1) {
+        /* display: block - current icon/text pair */
+        setDisplay(i);
+        /* wait 3 seconds */
+        /* display: none - current icon/text pair */
+        // setTimeout(function() {
+        //     document.getElementById("section1").style.display="none";
+        //     }, 3000);
+        i++;
+    }
+}
+
+function setDisplay(i) {
+        var icon = document.getElementById("icon" + i);
+        icon.style.display = "block";
+        var text = document.getElementById("text" + i);
+        text.style.display = "block";
+}
+
+function removeDisplay(i) {
+    document.getElementById("section1").style.display="none";
+
+    // var icon = document.getElementById("icon" + i);
+    // icon.style.display = "none";
+    // var text = document.getElementById("text" + i);
+    // text.style.display = "none";
+}
+
+
+/*
+==================================================
+
+Section 4
+
+==================================================
+*/
+
+/* --- Event listener to listen for hash change ---  */
 
 function checkSection4() {
     if (location.hash == '#impact') {
@@ -211,7 +155,6 @@ References:
 
 */
 
-
 var replayButton = document.getElementById('replay');
 
 // Remove and re-add content of section4 to re-start animations
@@ -227,6 +170,82 @@ function removeAddSection4() {
     section4.appendChild(removed);
 }
 
+
+/*
+==================================================
+
+Section 5 Navbar
+
+==================================================
+*/
+
+var sec5Nav = document.getElementById('section5-nav');        // reference to navbar element node
+
+function changeSec5Background(e) {
+  var sec5 = document.getElementById('current-projects');                 // reference to section 5 element node
+  var sec5NavTabs = sec5Nav.getElementsByTagName('li');           // NodeList for section 5 navbar tabs
+  var sec5SubSections = sec5.getElementsByTagName('section');     // NodeList for section 5 subsections
+  var target = e.target;
+  var elParent = target.parentNode;
+  for (let i = 0; i < sec5NavTabs.length; i++) {
+    if ((target.id == sec5NavTabs[i].id) || (elParent.id == sec5NavTabs[i].id)) {     // If ID of targer OR target's parent element (if user clicks on image)
+      sec5NavTabs[i].className = 'nav5-itemsClear';                                   // change background color of subsection's tab
+      sec5SubSections[i].className = 'section5ContentContainer'                       // render active subsection's html content visible
+      sec5.className = sec5SubSections[i].id + 'Bckgd';                               // modify's class to change to active subsection's background image
+    } else {                                                                          // Otherwise:
+      sec5NavTabs[i].className = 'nav5-itemsBlue';                                    // apply default color to inactive navbar tabs
+      sec5SubSections[i].className = 'sec5Hidden';                                    // render inactive subsections' HTML hidden
+    }
+  }
+}
+
+
+/*
+==================================================
+
+Section 6 Navbar
+
+==================================================
+*/
+
+const section6Nav = document.getElementById('section6-nav');
+
+function changeBackground(event) {
+    let lis = section6Nav.getElementsByTagName('li');
+    for (let i = 0; i < lis.length; i++) {
+        lis[i].style.backgroundColor = 'white';
+    }
+    let parentTarget = event.target.parentElement;
+    if (parentTarget.tagName != 'LI') {
+        parentTarget = parentTarget.parentElement;
+    }
+    parentTarget.style.backgroundColor = 'gold';
+    displayContent(parentTarget);
+}
+
+function displayContent(parentTarget) {
+    var section6 = document.getElementById('section6');
+    var subSections = section6.getElementsByTagName('section');
+    for (let i = 0; i < subSections.length; i++) {
+        let sectionId = subSections[i].id;
+        if (parentTarget.id == sectionId + '-nav') {
+            document.getElementById(sectionId).style.display = 'flex';
+        }
+        else {
+            document.getElementById(sectionId).style.display = 'none';
+        }
+    }
+}
+
+
+/*
+==================================================
+
+Event Listeners
+
+==================================================
+*/
+
 function setEventListeners() {
     carousel.addEventListener('click', function(e) {
         changeCarouselColors(e);}, false);
@@ -238,7 +257,6 @@ function setEventListeners() {
         changeSec5Background(e);}, false);
     section6Nav.addEventListener('click', function(event) {
         changeBackground(event);});
-
 }
 
 setEventListeners();
